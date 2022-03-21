aa11eae706014490cc72a740cc69e88954ff97d1
AleS900 <santiavctf@gmail.com>
2022-03-21 18:08:29 -0400

    Register Screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { Test1Component } from './test1/test1.component';
 import { Test2Component } from './test2/test2.component';
 import { Test3Component } from './test3/test3.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { LoginComponent } from './login/login.component';
     Test2Component,
     Test3Component,
     LoginComponent,
+    RegisterComponent,
     Directive1Directive,
     Directive2Directive,
   ],
