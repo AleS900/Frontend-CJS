295dc4d513ac5058b08b1f2d8544984ceaa24581
AleS900 <santiavctf@gmail.com>
2022-03-22 19:12:44 -0400

    3 files modified

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,16 +1,71 @@
-import {Component, Input} from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'register-comp',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
-
-export class RegisterComponent{
+export class RegisterComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    OnDestroy,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked
+{
   @Input() name;
 
   constructor() {
     console.log('CONSTRUCTOR');
   }
 
+  myName = 'Default Name';
+  myVarName = 'Nombre de Usuario o Correo';
+  myVarLastName = 'Contraseña';
+
+  ngOnChanges(changes: any) {
+    console.log('ON CHANGES', changes);
+  }
+
+  ngOnInit() {
+    console.log('ON INIT');
+  }
+
+  ngDoCheck() {
+    console.log('DO CHECK');
+  }
+
+  ngOnDestroy() {
+    console.log('ON DESTROY');
+  }
+
+  ngAfterContentInit() {
+    console.log('CONTENT INIT');
+  }
+
+  ngAfterContentChecked() {
+    console.log('CONTENT CHECKED');
+  }
+
+  ngAfterViewInit() {
+    console.log('VIEW INIT');
+  }
+
+  ngAfterViewChecked() {
+    console.log('VIEW CHECKED');
+  }
 }
