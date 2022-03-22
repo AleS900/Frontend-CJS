806c3f3e7777ed100e2547af9533456318c66ff0
AleS900 <santiavctf@gmail.com>
2022-03-22 18:42:05 -0400

    4 files modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,32 +1,11 @@
-import {
-  AfterContentChecked,
-  AfterContentInit,
-  AfterViewChecked,
-  AfterViewInit,
-  Component,
-  DoCheck,
-  Input,
-  OnChanges,
-  OnDestroy,
-  OnInit,
-} from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'login-comp',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent
-  implements
-    OnInit,
-    OnChanges,
-    DoCheck,
-    OnDestroy,
-    AfterContentInit,
-    AfterContentChecked,
-    AfterViewInit,
-    AfterViewChecked
-{
+export class LoginComponent {
   @Input() name;
 
   constructor() {
@@ -41,37 +20,5 @@ export class LoginComponent
     console.log('Event click: ', event);
     console.log('DATA: ', this.myVarName + ' ' + this.myVarLastName);
     this.myName = this.myVarName + ' ' + this.myVarLastName;
-  }
-
-  ngOnChanges(changes: any) {
-    console.log('ON CHANGES', changes);
-  }
-
-  ngOnInit() {
-    console.log('ON INIT');
-  }
-
-  ngDoCheck() {
-    console.log('DO CHECK');
-  }
-
-  ngOnDestroy() {
-    console.log('ON DESTROY');
-  }
-
-  ngAfterContentInit() {
-    console.log('CONTENT INIT');
-  }
-
-  ngAfterContentChecked() {
-    console.log('CONTENT CHECKED');
-  }
-
-  ngAfterViewInit() {
-    console.log('VIEW INIT');
-  }
-
-  ngAfterViewChecked() {
-    console.log('VIEW CHECKED');
   }
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,4 @@
-import {
-  AfterContentChecked,
-  AfterContentInit,
-  AfterViewChecked,
-  AfterViewInit,
-  Component,
-  DoCheck,
-  Input,
-  OnChanges,
-  OnDestroy,
-  OnInit,
-} from '@angular/core';
+import { Component,Input} from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
@@ -18,62 +7,11 @@ import {MatToolbarModule} from '@angular/material/toolbar';
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css'],
 })
-export class MainComponent
-  implements
-    OnInit,
-    OnChanges,
-    DoCheck,
-    OnDestroy,
-    AfterContentInit,
-    AfterContentChecked,
-    AfterViewInit,
-    AfterViewChecked
-{
+export class MainComponent{
   @Input() name;
 
   constructor() {
     console.log('CONSTRUCTOR');
   }
 
-  myName = 'Default Name';
-  myVarName = 'Nombre de Usuario o Correo';
-  myVarLastName = 'Contraseña';
-
-  test3(event) {
-    console.log('Event click: ', event);
-    console.log('DATA: ', this.myVarName + ' ' + this.myVarLastName);
-    this.myName = this.myVarName + ' ' + this.myVarLastName;
-  }
-
-  ngOnChanges(changes: any) {
-    console.log('ON CHANGES', changes);
-  }
-
-  ngOnInit() {
-    console.log('ON INIT');
-  }
-
-  ngDoCheck() {
-    console.log('DO CHECK');
-  }
-
-  ngOnDestroy() {
-    console.log('ON DESTROY');
-  }
-
-  ngAfterContentInit() {
-    console.log('CONTENT INIT');
-  }
-
-  ngAfterContentChecked() {
-    console.log('CONTENT CHECKED');
-  }
-
-  ngAfterViewInit() {
-    console.log('VIEW INIT');
-  }
-
-  ngAfterViewChecked() {
-    console.log('VIEW CHECKED');
-  }
 }
